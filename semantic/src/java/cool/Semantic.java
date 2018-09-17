b84bf8b653cd54ba29015f4476c91275fae05e53
r@MKishAn <35762247+RamkishanPOTHUGANTI@users.noreply.github.com>
2018-09-17 14:53:46 +0530

    added work files

---
+++ semantic/src/java/cool/Semantic.java
@@ -0,0 +1,19 @@
+package cool;
+
+public class Semantic{
+	private boolean errorFlag = false;
+	public void reportError(String filename, int lineNo, String error){
+		errorFlag = true;
+		System.err.println(filename+":"+lineNo+": "+error);
+	}
+	public boolean getErrorFlag(){
+		return errorFlag;
+	}
+
+/*
+	Don't change code above this line
+*/
+	public Semantic(AST.program program){
+		//Write Semantic analyzer code here
+	}
+}
